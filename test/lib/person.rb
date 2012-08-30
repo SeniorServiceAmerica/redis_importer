9abeb25c8db0217439ca952e1ccfc63d1a3b1cf0
Ian Whitney <ian@ianwhitney.com>
2012-08-30 10:38:54 -0500

    Updating example model to work with update to csv_to_object. Setting new dependency level for the csv_to_object gem

--- test/lib/person.rb
+++ test/lib/person.rb
@@ -2,9 +2,9 @@ class Person
   attr_reader :id, :first_name, :last_name
 
   def initialize(args={})
-    @id = args[:id]
-    @first_name = args[:first_name]
-    @last_name = args[:last_name]
+    @id = args["id"]
+    @first_name = args["first_name"]
+    @last_name = args["last_name"]
   end
   
   def to_redis()
