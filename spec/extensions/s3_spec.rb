123caaf4606f772371f2031a60fe73d5f5cfdb38
Davin Lagerroos <dlagerroos@ssa-i.org>
2013-10-18 17:44:10 -0500

    update S3Object#to_class_name extension to handle snake_case to CamelCase conversions

---
+++ spec/extensions/s3_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+module AWS
+  module S3
+    describe S3Object do
+      before(:each) do
+        @s3 = S3Object.new
+      end
+      describe "to_class_name" do
+        it "removes the .csv from the end of the name" do
+          @s3.stub(:key).and_return('something.csv')
+          @s3.to_class_name.should_not include('.csv')
+        end
+        it "capitalizes the name" do
+          @s3.stub(:key).and_return('something.csv')
+          @s3.to_class_name.should eq 'Something'
+        end
+        it "converts snake_case to CamelCase" do
+          @s3.stub(:key).and_return('multi_word_class.csv')
+          @s3.to_class_name.should eq 'MultiWordClass'
+        end
+      end
+    end
+  end
+end
